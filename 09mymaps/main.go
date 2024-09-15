903d52d0b81c3810a48e7f78ac38bbf123f7cbdd
Jyothis Benny <jyothis@edgebricks.com>
2024-09-15 09:38:11 +0530

    maps

--- 09mymaps/main.go
+++ 09mymaps/main.go
@@ -1,1 +1,24 @@
-package _9mymaps
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("my maps")
+	names := make(map[string]string)
+	fmt.Println(names)
+
+	names["JY"] = "jyothis"
+	names["AN"] = "anash"
+	names["KR"] = "krishna"
+
+	fmt.Println(names)
+
+	delete(names, "KR")
+	fmt.Println(names)
+
+	for sn, fullName := range names {
+		fmt.Println(sn, fullName)
+	}
+}
