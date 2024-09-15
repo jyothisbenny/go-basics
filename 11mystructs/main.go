36b72a0ac432a4e0fb2ec48570856b52511d6d58
Jyothis Benny <jyothis@edgebricks.com>
2024-09-15 09:37:31 +0530

    structs

--- 11mystructs/main.go
+++ 11mystructs/main.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Structs")
+	//there is no inheritance in go; so super or parent
+	user := User{"Jyothis", 20, true, "jyothis@gmail.com"}
+	fmt.Println(user)
+	fmt.Printf("user details are %+v \n", user)
+	fmt.Printf("user name:%v, email:%v \n", user.Name, user.Email)
+}
+
+type User struct {
+	Name   string
+	Age    int
+	Status bool
+	Email  string
+}
